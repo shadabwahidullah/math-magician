b2ec44b6863d30818ba6c412de8c66f483211602
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-27 03:07:17 +0430

    Change function base component to class base component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,38 @@
 import '../Calculator.css';
 import React from 'react';
 
-const Calculator = () => (
-  <div className="wrapper">
-    <p className="result">0</p>
-    <p className="key">AC</p>
-    <p className="key">+/-</p>
-    <p className="key">%</p>
-    <p className="key operation">/</p>
-    <p className="key">7</p>
-    <p className="key">8</p>
-    <p className="key">9</p>
-    <p className="key operation">x</p>
-    <p className="key">4</p>
-    <p className="key">5</p>
-    <p className="key">6</p>
-    <p className="key operation">-</p>
-    <p className="key">1</p>
-    <p className="key">2</p>
-    <p className="key">3</p>
-    <p className="key operation">+</p>
-    <p className="key zero">0</p>
-    <p className="key">.</p>
-    <p className="key operation">=</p>
-  </div>
-);
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <p className="result">0</p>
+        <p className="key">AC</p>
+        <p className="key">+/-</p>
+        <p className="key">%</p>
+        <p className="key operation">/</p>
+        <p className="key">7</p>
+        <p className="key">8</p>
+        <p className="key">9</p>
+        <p className="key operation">x</p>
+        <p className="key">4</p>
+        <p className="key">5</p>
+        <p className="key">6</p>
+        <p className="key operation">-</p>
+        <p className="key">1</p>
+        <p className="key">2</p>
+        <p className="key">3</p>
+        <p className="key operation">+</p>
+        <p className="key zero">0</p>
+        <p className="key">.</p>
+        <p className="key operation">=</p>
+      </div>
+    );
+  }
+}
 
 export default Calculator;
