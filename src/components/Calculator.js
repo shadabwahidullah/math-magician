a1cfe85f9459a265699236d5601e118a0b7cd5cf
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-28 23:59:12 +0430

    Refactor class based component to fuction based component, Added hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,120 +1,109 @@
 import '../Calculator.css';
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      obj: { total: 0, next: null, operation: null },
-    };
+function Calculator() {
+  const [obj, setObj] = useState({ total: 0, next: null, operation: null });
+
+  function onClickListener(event) {
+    setObj(calculate(obj, event.target.innerText));
   }
 
-  onClickListener = (event) => {
-    const { obj } = this.state;
-    this.setState({
-      obj: calculate(obj, event.target.innerText),
-    });
-  };
-
-  render() {
-    const { obj } = this.state;
-    return (
-      <div className="wrapper">
-        <p className="result">
-          {obj.next}
-          {obj.operation}
-          {obj.total}
-        </p>
-        <button
-          type="button"
-          className="key"
-          onClick={this.onClickListener}
-          name="AC"
-        >
-          AC
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          +/-
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          %
-        </button>
-        <button
-          type="button"
-          className="key operation"
-          onClick={this.onClickListener}
-        >
-          /
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          7
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          8
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          9
-        </button>
-        <button
-          type="button"
-          className="key operation"
-          onClick={this.onClickListener}
-        >
-          x
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          4
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          5
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          6
-        </button>
-        <button
-          type="button"
-          className="key operation"
-          onClick={this.onClickListener}
-        >
-          -
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          1
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          2
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          3
-        </button>
-        <button
-          type="button"
-          className="key operation"
-          onClick={this.onClickListener}
-        >
-          +
-        </button>
-        <button
-          type="button"
-          className="key zero"
-          onClick={this.onClickListener}
-        >
-          0
-        </button>
-        <button type="button" className="key" onClick={this.onClickListener}>
-          .
-        </button>
-        <button
-          type="button"
-          className="key operation"
-          onClick={this.onClickListener}
-        >
-          =
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="wrapper">
+      <p className="result">
+        {obj.total}
+        {obj.operation}
+        {obj.next}
+      </p>
+      <button
+        type="button"
+        className="key"
+        onClick={onClickListener}
+        name="AC"
+      >
+        AC
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        +/-
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        %
+      </button>
+      <button
+        type="button"
+        className="key operation"
+        onClick={onClickListener}
+      >
+        /
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        7
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        8
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        9
+      </button>
+      <button
+        type="button"
+        className="key operation"
+        onClick={onClickListener}
+      >
+        x
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        4
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        5
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        6
+      </button>
+      <button
+        type="button"
+        className="key operation"
+        onClick={onClickListener}
+      >
+        -
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        1
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        2
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        3
+      </button>
+      <button
+        type="button"
+        className="key operation"
+        onClick={onClickListener}
+      >
+        +
+      </button>
+      <button
+        type="button"
+        className="key zero"
+        onClick={onClickListener}
+      >
+        0
+      </button>
+      <button type="button" className="key" onClick={onClickListener}>
+        .
+      </button>
+      <button
+        type="button"
+        className="key operation"
+        onClick={onClickListener}
+      >
+        =
+      </button>
+    </div>
+  );
 }
 
 export default Calculator;
