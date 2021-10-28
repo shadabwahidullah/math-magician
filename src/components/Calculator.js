64c407f3bf614540152524ce4639e5e77806dc61
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-10-29 00:25:54 +0430

    Refactor ES5 functions to ES6 arrow functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,12 +2,12 @@ import '../Calculator.css';
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-function Calculator() {
+const Calculator = () => {
   const [obj, setObj] = useState({ total: 0, next: null, operation: null });
 
-  function onClickListener(event) {
+  const onClickListener = (event) => {
     setObj(calculate(obj, event.target.innerText));
-  }
+  };
 
   return (
     <div className="wrapper">
@@ -16,12 +16,7 @@ function Calculator() {
         {obj.operation}
         {obj.next}
       </p>
-      <button
-        type="button"
-        className="key"
-        onClick={onClickListener}
-        name="AC"
-      >
+      <button type="button" className="key" onClick={onClickListener} name="AC">
         AC
       </button>
       <button type="button" className="key" onClick={onClickListener}>
@@ -30,11 +25,7 @@ function Calculator() {
       <button type="button" className="key" onClick={onClickListener}>
         %
       </button>
-      <button
-        type="button"
-        className="key operation"
-        onClick={onClickListener}
-      >
+      <button type="button" className="key operation" onClick={onClickListener}>
         /
       </button>
       <button type="button" className="key" onClick={onClickListener}>
@@ -46,11 +37,7 @@ function Calculator() {
       <button type="button" className="key" onClick={onClickListener}>
         9
       </button>
-      <button
-        type="button"
-        className="key operation"
-        onClick={onClickListener}
-      >
+      <button type="button" className="key operation" onClick={onClickListener}>
         x
       </button>
       <button type="button" className="key" onClick={onClickListener}>
@@ -62,11 +49,7 @@ function Calculator() {
       <button type="button" className="key" onClick={onClickListener}>
         6
       </button>
-      <button
-        type="button"
-        className="key operation"
-        onClick={onClickListener}
-      >
+      <button type="button" className="key operation" onClick={onClickListener}>
         -
       </button>
       <button type="button" className="key" onClick={onClickListener}>
@@ -78,32 +61,20 @@ function Calculator() {
       <button type="button" className="key" onClick={onClickListener}>
         3
       </button>
-      <button
-        type="button"
-        className="key operation"
-        onClick={onClickListener}
-      >
+      <button type="button" className="key operation" onClick={onClickListener}>
         +
       </button>
-      <button
-        type="button"
-        className="key zero"
-        onClick={onClickListener}
-      >
+      <button type="button" className="key zero" onClick={onClickListener}>
         0
       </button>
       <button type="button" className="key" onClick={onClickListener}>
         .
       </button>
-      <button
-        type="button"
-        className="key operation"
-        onClick={onClickListener}
-      >
+      <button type="button" className="key operation" onClick={onClickListener}>
         =
       </button>
     </div>
   );
-}
+};
 
 export default Calculator;
