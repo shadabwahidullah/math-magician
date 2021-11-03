028c7380191646b85d791951530c64a49bd0fe44
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-03 04:40:22 +0430

    Design Calculator page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,37 @@
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <div>
-    <h1>Math Magicians</h1>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/calculator">Calculator</Link>
-      </li>
-      <li>
-        <Link to="/Quote">Quote</Link>
-      </li>
-    </ul>
-  </div>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
+    <div className="d-flex col-12 justify-content-between">
+      <Link to="/" className="navbar-brand">
+        Math Magicians
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link to="/" className="nav-link active">
+            Home
+          </Link>
+          <Link to="/calculator" className="nav-link">
+            Calculator
+          </Link>
+          <Link to="/Quote" className="nav-link">
+            Quote
+          </Link>
+        </div>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Header;
